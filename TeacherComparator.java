32879d520745d1d23cc3a01f540f637f04b5b30b
Aleksandr <alexlev83@list.ru>
2024-01-15 14:49:14 +0300

    addTeacherComparator

---
+++ TeacherComparator.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+
+public class TeacherComparator <T extends Teacher> implements Comparator<T> {
+
+    @Override
+    public int compare(T o1, T o2) {
+        int resultOfComparing = o1.getSecondName().compareTo(o2.getFirstName());
+        if (resultOfComparing == 0) {
+            resultOfComparing = o1.getFirstName().compareTo(o2.getFirstName());
+            if (resultOfComparing == 0) {
+                return o1.getLasttName().compareTo(o2.getLasttName());
+            } else {
+                return resultOfComparing;
+            }
+        } else {
+            return resultOfComparing;
+        }
+    }
+}
